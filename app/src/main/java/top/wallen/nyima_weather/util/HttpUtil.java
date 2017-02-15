9105b5104b14b4f05e663b1f7fa9c85249ef1607
wallena3 <wallena3@gmail.com>
2017-02-15 13:26:21 +0800

    update the conf of database & table.

---
+++ app/src/main/java/top/wallen/nyima_weather/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package top.wallen.nyima_weather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by walle on 2/15/17.
+ */
+
+public class HttpUtil {
+
+    public  static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
